4425ec8abc5fde62e231345bccae7b0085be68ce
Artur Kania <kaniartur@gmail.com>
2014-05-28 18:27:37 +0200

    fixed test for report feature

--- tests/features/step_definitions/reportFeature_steps.js
+++ tests/features/step_definitions/reportFeature_steps.js
@@ -1,6 +1,36 @@
 CucumberJsBrowserRunnerStepDefinitions.reportFeature(function () {
 
-    var And = Given = When = Then = this.defineStep;
+    var And = Given = When = Then = this.defineStep,
+        featureName = '',
+        featureCode = {},
+        runner;
 
+    this.Before(function(beforething, callback) {
+        runner = new CucumberJsBrowserRunner();
+        callback();
+    });
 
+    Given(/^a web page$/, function(callback) {
+        callback();
+    });
+
+    When(/^I load a feature '(\w+)'$/, function(feature, callback) {
+        runner.loadFeatures(feature, function (runner) {
+            featureCode[feature] = feature;
+            callback();
+        }, callback.fail);
+    });
+
+    And(/^I run feature '(\w+)'$/, function (feature, callback) {
+        runner.setOutput('console');
+        runner.run(feature);
+    });
+    
+    Then(/^i can see in report that feature test '(\w+)'$/, function(status, callback) {
+        if (status === runner.getReport().features[0].status) {
+            callback();
+        } else {
+            callback.fail();
+        }
+    });
 });
